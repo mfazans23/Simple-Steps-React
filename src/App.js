bec9c77cf743812106be1ee0d76d90c98c11520b
mfazans23 <mfaza16717@mail.ugm.ac.id>
2023-07-27 13:55:29 +0700

    add files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+
+const messages = [
+  'Learn React ⚛️',
+  'Apply for jobs 💼',
+  'Invest your new income 🤑',
+]
 
 function App() {
+  const [step, setStep] = useState(1)
+  const [isOpen, setIsOpen] = useState(true)
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1)
+  }
+
+  function handleNext() {
+    if (step < messages.length) setStep((s) => s + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <button className='close' onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
+      {isOpen ? (
+        <div className='steps'>
+          <div className='numbers'>
+            {[...Array(messages.length).keys()].map((index) => (
+              <div
+                key={index}
+                className={`${step >= index + 1 ? 'active' : ''}`}
+              >
+                {index + 1}
+              </div>
+            ))}
+          </div>
+          <p className='message'>
+            Step {step}: {messages[step - 1]}
+          </p>
+          <div className='buttons'>
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: '#7950f2', color: '#fff' }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      ) : null}
+    </>
+  )
 }
 
-export default App;
+export default App
